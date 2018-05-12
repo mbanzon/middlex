e0012b428743579106c26bfca65a9e7caa0078d6
Michael Banzon <michael@banzon.dk>
2018-05-12 14:25:30 +0200

    Added middleware that prevents caching (by telling the browser not to cache)

---
+++ nocache/nocache.go
@@ -0,0 +1,21 @@
+package nocache
+
+import (
+	"github.com/mbanzon/middlex"
+	"github.com/mbanzon/middlex/header"
+)
+
+type NoCache struct{}
+
+func New() *NoCache {
+	return &NoCache{}
+}
+
+func (n *NoCache) Middleware() middlex.Middleware {
+	noCacheHeaders := make(map[string]string)
+	noCacheHeaders["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	noCacheHeaders["Pragma"] = "no-cache"
+	noCacheHeaders["Expires"] = "0"
+
+	return header.New(noCacheHeaders).Middleware()
+}
