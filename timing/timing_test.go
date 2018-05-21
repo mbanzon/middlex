10a16c394d3659c2d0ebbcc6133e7dcea70f49f5
Michael Banzon <michael@banzon.dk>
2018-05-21 11:20:58 +0200

    Added test

--- timing/timing_test.go
+++ timing/timing_test.go
@@ -10,8 +10,11 @@ import (
 func TestSingleTimer(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 
+	var totalTime time.Duration
+
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		d := time.Duration(rand.Int63n(100)) * time.Millisecond
+		totalTime += d
 		time.Sleep(d)
 	})
 
@@ -19,11 +22,28 @@ func TestSingleTimer(t *testing.T) {
 	wrapper := timer.Middleware()
 	wrapped := wrapper(handler)
 
-	t.Log(timer.Avg())
-
 	for i := 0; i < 100; i++ {
 		wrapped.ServeHTTP(nil, nil)
 	}
 
+	count, avg := timer.Avg()
+	if count != 100 {
+		t.Fatal("unexptected count:", count)
+	}
+
+	if avg < totalTime/100 {
+		t.Fatal("avg too low:", avg)
+	}
+
 	timer.Reset()
+
+	count, avg = timer.Avg()
+
+	if count != 0 {
+		t.Fatal("unexpected count after reset:", count)
+	}
+
+	if avg != 0 {
+		t.Fatal("unexpected avg after reset:", avg)
+	}
 }
