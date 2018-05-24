958707bc92fe902f906d689c5d4aca83a4aee369
Michael Banzon <michael@banzon.dk>
2018-05-24 05:35:11 +0200

    Added Wrapper function to wrap multiple Middleware

--- middleware.go
+++ middleware.go
@@ -3,3 +3,20 @@ package middlex
 import "net/http"
 
 type Middleware func(http.Handler) http.Handler
+
+type MiddlewareFactory interface {
+	Middleware() Middleware
+}
+
+func Wrapper(factories ...MiddlewareFactory) Middleware {
+	return func(h http.Handler) http.Handler {
+		wrapped := h
+		for _, f := range factories {
+			wrapped = f.Middleware()(wrapped)
+		}
+
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			wrapped.ServeHTTP(w, r)
+		})
+	}
+}
