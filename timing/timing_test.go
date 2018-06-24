ebbf6adbdff7d0de282c9020f9f4a65301f53f27
Michael Banzon <michael@banzon.dk>
2018-06-24 10:55:17 +0200

    Added tests for timing

--- timing/timing_test.go
+++ timing/timing_test.go
@@ -3,6 +3,7 @@ package timing
 import (
 	"math/rand"
 	"net/http"
+	"sync"
 	"testing"
 	"time"
 )
@@ -46,4 +47,98 @@ func TestSingleTimer(t *testing.T) {
 	if avg != 0 {
 		t.Fatal("unexpected avg after reset:", avg)
 	}
+}
+
+func TestTimerWithEmitter(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+
+	var totalTime time.Duration
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		d := time.Duration(rand.Int63n(100)) * time.Millisecond
+		totalTime += d
+		time.Sleep(d)
+	})
+
+	done := false
+	wg := sync.WaitGroup{}
+	runEmitFn := true
+
+	emitFn := func(ti time.Time, count int64, d time.Duration) {
+		if runEmitFn {
+			if done {
+				if count != 100 {
+					t.Fatal("expected count to be 100:", count)
+				}
+				wg.Done()
+			}
+		}
+	}
+
+	wg.Add(1)
+
+	timer := New(WithEmitter(emitFn, 200*time.Millisecond))
+	wrapper := timer.Middleware()
+	wrapped := wrapper(handler)
+
+	for i := 0; i < 100; i++ {
+		wrapped.ServeHTTP(nil, nil)
+	}
+
+	done = true
+	wg.Wait()
+	runEmitFn = false
+
+	if timer.count != 100 {
+		t.Fatal("expected count to be 100:", timer.count)
+	}
+}
+
+func TestTimerWithEmitReset(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+
+	var totalTime time.Duration
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		d := time.Duration(rand.Int63n(100)) * time.Millisecond
+		totalTime += d
+		time.Sleep(d)
+	})
+
+	done := false
+	wg := sync.WaitGroup{}
+	var totalCount int64
+	runEmitFn := true
+
+	emitFn := func(ti time.Time, count int64, d time.Duration) {
+		if runEmitFn {
+			totalCount += count
+			if done {
+				wg.Done()
+			}
+		}
+	}
+
+	wg.Add(1)
+
+	timer := New(WithResetEmitter(emitFn, 200*time.Millisecond))
+	wrapper := timer.Middleware()
+	wrapped := wrapper(handler)
+
+	for i := 0; i < 100; i++ {
+		wrapped.ServeHTTP(nil, nil)
+	}
+
+	done = true
+	wg.Wait()
+	runEmitFn = false
+
+	if timer.count != 0 {
+		t.Fatal("expected count to be 0:", timer.count)
+	}
+
+	if totalCount != 100 {
+		t.Fatal("expected total count to be 100:", totalCount)
+	}
+
 }
