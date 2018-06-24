e7334deaf8e1b90236c3357cac0d47850552fd91
Michael Banzon <michael@banzon.dk>
2018-06-24 07:21:08 +0200

    Adding comments

--- middleware.go
+++ middleware.go
@@ -2,12 +2,20 @@ package middlex
 
 import "net/http"
 
+// Middleware describes the function used to wrap a http.Handler in another
+// to apply middleware.
 type Middleware func(http.Handler) http.Handler
 
+// MiddlewareFactory describes the interface used to create Middleware
+// instances.
 type MiddlewareFactory interface {
 	Middleware() Middleware
 }
 
+// Combine takes a number of MiddlewareFactory and use them to combine them
+// into a single Middleware. Please note that on every invokation of the
+// resulting Middleware the factories are used to produce new Middleware
+// instances.
 func Combine(factories ...MiddlewareFactory) Middleware {
 	return func(h http.Handler) http.Handler {
 		wrapped := h
