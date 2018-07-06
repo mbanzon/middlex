445d6024635883a2b4643fbbfe662eac57647b25
Michael Banzon <michael@banzon.dk>
2018-07-06 10:21:48 +0200

    Added emit

---
+++ usercount/emit.go
@@ -0,0 +1,20 @@
+package usercount
+
+import (
+	"time"
+)
+
+type CountEmitFunc func(time.Time, int64)
+
+func WithEmitFunction(cef CountEmitFunc, every time.Duration) ConfigFunc {
+	return func(u *UserCount) {
+		go func() {
+			tick := time.Tick(every)
+			for now := range tick {
+				u.mutex.Lock()
+				defer u.mutex.Unlock()
+				cef(now, int64(len(u.counts)))
+			}
+		}()
+	}
+}
