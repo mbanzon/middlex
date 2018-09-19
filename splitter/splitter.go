1c31e95dfb003d439bc63efe73386744c8f56ce6
Michael Banzon <michael@banzon.dk>
2018-09-19 17:50:00 +0200

    Splitting by URI element

---
+++ splitter/splitter.go
@@ -0,0 +1,84 @@
+package splitter
+
+import (
+	"net/http"
+	"strings"
+)
+
+type Splitter struct {
+	prefix         string
+	splits         map[string]http.Handler
+	defaultHandler http.Handler
+}
+
+type ConfigFunc func(*Splitter)
+
+func New(config ...ConfigFunc) *Splitter {
+	sh := &Splitter{
+		splits: make(map[string]http.Handler),
+	}
+
+	for _, confFn := range config {
+		confFn(sh)
+	}
+
+	return sh
+}
+
+func WithPrefix(prefix string) ConfigFunc {
+	return func(sh *Splitter) {
+		sh.prefix = prefix
+	}
+}
+
+func WithSplit(path string, h http.Handler) ConfigFunc {
+	return func(sh *Splitter) {
+		sh.splits[path] = h
+	}
+}
+
+func WithDefaultHandler(h http.Handler) ConfigFunc {
+	return func(sh *Splitter) {
+		sh.defaultHandler = h
+	}
+}
+
+func (sh *Splitter) Handler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.RequestURI, sh.prefix) {
+			originalURI := r.RequestURI
+			r.RequestURI = r.RequestURI[len(sh.prefix):]
+			path := popURIArg(r)
+			hitHandler := false
+			for p, h := range sh.splits {
+				if path == p {
+					hitHandler = true
+					h.ServeHTTP(w, r)
+				}
+			}
+			r.RequestURI = originalURI
+			if !hitHandler && sh.defaultHandler != nil {
+				sh.defaultHandler.ServeHTTP(w, r)
+			}
+		}
+	})
+}
+
+func popURIArg(r *http.Request) string {
+	uri := strings.Trim(r.RequestURI, "/")
+
+	if uri == "" {
+		return ""
+	}
+
+	var arg string
+	index := strings.Index(uri, "/")
+	if index != -1 {
+		arg = uri[:index]
+		r.RequestURI = uri[index:]
+	} else {
+		arg = uri
+		r.RequestURI = ""
+	}
+	return arg
+}
