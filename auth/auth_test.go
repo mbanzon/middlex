3f2383d50c7a513374e44082e3b47d2ee908f58f
Michael Banzon <michael@banzon.dk>
2019-02-22 08:18:12 +0100

    Improving tests

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -12,6 +12,42 @@ func TestNewAuth(t *testing.T) {
 	wrapped.ServeHTTP(w, r)
 }
 
+func TestOptionsNoAccess(t *testing.T) {
+	count := 0
+	wrapped := setupWrapped(t, func() {
+		count++
+	})
+	req, _ := http.NewRequest(http.MethodOptions, "", nil)
+	w := httptest.NewRecorder()
+	wrapped.ServeHTTP(w, req)
+
+	if count != 0 {
+		t.Fatal()
+	}
+
+	if w.Code != http.StatusUnauthorized {
+		t.Fatal()
+	}
+}
+func TestOptionsAccess(t *testing.T) {
+	count := 0
+	wrapped := setupWrapped(t, func() {
+		count++
+	},
+		WithOptionsAcceptance())
+	req, _ := http.NewRequest(http.MethodOptions, "", nil)
+	w := httptest.NewRecorder()
+	wrapped.ServeHTTP(w, req)
+
+	if count != 1 {
+		t.Fatal()
+	}
+
+	if w.Code != http.StatusOK {
+		t.Fatal()
+	}
+}
+
 func TestWithCookieExtractor(t *testing.T) {
 	c := WithCookieTokenExtraction("my-cookie")
 	wrapped, w, r := setupEmptyWrapped(t, http.MethodGet, "/", nil, c)
@@ -30,4 +66,15 @@ func setupEmptyWrapped(t *testing.T, method, url string, data io.Reader, c ...Co
 	r := httptest.NewRequest(method, url, data)
 
 	return wrapped, w, r
+}
+
+func setupWrapped(t *testing.T, f func(), c ...ConfigFunc) http.Handler {
+	t.Helper()
+
+	empty := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		f()
+	})
+	auther := New(c...)
+	wrapped := auther.Middleware()(empty)
+	return wrapped
 }
